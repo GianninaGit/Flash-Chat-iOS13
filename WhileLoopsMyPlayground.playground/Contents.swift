e22a9f23592879acf00bd9ba08dd6cdf6951718e
Giannina <giannisarappa@gmail.com>
2023-06-24 15:20:19 +0200

    While loops

---
+++ WhileLoopsMyPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+import Foundation
+
+var now = Date().timeIntervalSince1970
+let oneSecondFromNow = now + 1
+
+while now < oneSecondFromNow {
+    now = Date().timeIntervalSince1970
+    print("waiting...")
+}
