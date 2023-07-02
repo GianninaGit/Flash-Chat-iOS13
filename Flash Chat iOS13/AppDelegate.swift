e3dd672b601bcfbc3a1e8162ce005cad02e4b7c5
Giannina <giannisarappa@gmail.com>
2023-07-02 15:45:20 +0200

    Video 196. Parte 2, fix bottom of keyboard. How to Manage the Keyboard and use the Swift Package Manager

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -25,7 +25,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         IQKeyboardManager.shared.enable = true
         IQKeyboardManager.shared.enableAutoToolbar = false
-
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+        
         return true
     }
 
